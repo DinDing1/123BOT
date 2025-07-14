c026fbaefc8427ea5dbdcdcb53c91aecaf052f5e
DinDing1 <119488409+DinDing1@users.noreply.github.com>
2025-07-14 16:38:33 +0800

    Update and rename patch_asynctools.py to entrypoint.sh

--- patch_asynctools.py
+++
@@ -1,9 +0,0 @@
-# patch_asynctools.py
-from itertools import chain
-from asynctools import __all__ as original_all
-from asynctools import *
-
-async_chain_from_iterable = chain
-
-if 'async_chain_from_iterable' not in original_all:
-    original_all.append('async_chain_from_iterable')
